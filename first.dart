f34861927255286e7b11af9c930e9b85365234a2
KayesAshfaq <ashfaq7074@gmail.com>
2026-05-13 00:42:40 +0600

    initial implementation of first.dart with basic variables and main function

---
+++ first.dart
@@ -0,0 +1,14 @@
+var name = 'Jhon';
+int age = 23;
+dynamic x = 6.43434;
+final String country = 'Bangladesh';
+const double pi = 3.14159;
+
+String text = '''this is a\n text\n data''';
+
+String profession = "I'm a doctor";
+
+void main(List<String> args) {
+  print(text);
+  print(profession);
+}
